60044e54d5d51629866b7609b90ae66625ad3d18
chrisupt2001 <73676459+chrisupt2001@users.noreply.github.com>
2023-02-28 17:12:14 -0500

    v1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css'
-import Books from './components/Books'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div className="App">
-      <Books />
+      <Header />
     </div>
   )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,37 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 const conversions = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
+
+const Header = () =>
+{
+    const [apiCall, setApiCall] = useState({})
+
+useEffect (() => {
+
+    console.log('useEffect called')
+    axios.get(conversions)
+    .then(res => {
+        const convertData = res.data;
+        console.log(convertData.bpi.USD)
+        setApiCall({...convertData.bpi});
+})}, []);
+
+
+    return(
+        <>
+        <button onClick={()=>console.log(apiCall)}>test</button>
+        <h1>{apiCall.USD.rate}</h1>
+        <ul>
+  <li><a href="default.asp">Home</a></li>
+  <li><a href="news.asp">News</a></li>
+  <li><a href="contact.asp">Contact</a></li>
+  <li><a href="about.asp">About</a></li>
+</ul>
+</>
+    )
+
+}
+
+export default Header;
