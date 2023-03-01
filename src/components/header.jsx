f6d8f319db17c7a3b736045706880245541f570b
chrisupt2001 <73676459+chrisupt2001@users.noreply.github.com>
2023-03-01 16:30:24 -0500

    v1.2

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,13 @@ const conversions = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
 const Header = () =>
 {
-    const [apiCall, setApiCall] = useState({})
+    const [apiCall, setApiCall] = useState({
+        
+    })
+
+    const [USDRate, setUSDRate] = useState(0.0)
+    const [EURRate, setEURRate] = useState(0.0)
+    const [GBPRate, setGBPRate] = useState(0.0)
 
 useEffect (() => {
 
@@ -14,15 +20,18 @@ useEffect (() => {
     axios.get(conversions)
     .then(res => {
         const convertData = res.data;
-        console.log(convertData.bpi.USD)
+        console.log(convertData.bpi)
         setApiCall({...convertData.bpi});
+        setUSDRate(convertData.bpi.USD.rate)
+        setEURRate(convertData.bpi.EUR.rate)
+        setGBPRate(convertData.bpi.GBP.rate)
 })}, []);
 
 
     return(
         <>
         <button onClick={()=>console.log(apiCall)}>test</button>
-        <h1>{apiCall.USD.rate}</h1>
+        <h1>{USDRate}</h1>
         <ul>
   <li><a href="default.asp">Home</a></li>
   <li><a href="news.asp">News</a></li>
